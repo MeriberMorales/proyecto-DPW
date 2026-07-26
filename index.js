15fcea63a5be3779acc7bc162374935ef0ff20b2
Mery <meribercmorales@gmail.com>
2026-07-26 12:10:57 -0600

    feat: agregar index.js para leer variables de entorno

---
+++ index.js
@@ -0,0 +1,5 @@
+require('dotenv').config();
+
+console.log("Proyecto iniciado correctamente.");
+console.log("La variable PROMOCION fue leída desde el archivo .env:");
+console.log(process.env.PROMOCION);
